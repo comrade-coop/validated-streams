7f6bf6406052ad152daa1ab40182762733bcddfb
Ego_Renegade <eziorevoltez@gmail.com>
2022-10-16 06:13:50 +0100

    inital commi

---
+++ pallet/src/lib.rs
@@ -0,0 +1,18 @@
+// Re-export pallet items so that they can be accessed from the crate namespace.
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+  use frame_support::pallet_prelude::*;
+  use frame_system::pallet_prelude::*;
+
+  #[pallet::pallet]
+  #[pallet::generate_store(pub(super) trait Store)]
+  pub struct Pallet<T>(_);
+
+  #[pallet::config]
+  #[pallet::event]
+  #[pallet::error]
+  #[pallet::storage]
+  #[pallet::call]
+}
